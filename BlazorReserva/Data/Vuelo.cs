bbe9813abe14ad3574ef5e7a1eda6553cee368a3
JoseAugusto0426 <156604748+JoseAugusto0426@users.noreply.github.com>
2024-02-26 14:45:15 -0400

    Creo las Clases. y Los componente.razor

---
+++ BlazorReserva/Data/Vuelo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorReserva.Data
+{
+    public class Vuelo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+
+        public string Airport { get; set; } = "";
+        public DateTime Fecha { get; set; }
+        public ICollection<VueloDetalle> Detalles { get; set; }
+        = new HashSet<VueloDetalle>();
+        [NotMapped]
+        public decimal Total => (Detalles != null && Detalles.Any()) ? Detalles.Sum(d => d.SubTotal) : 0;
+    }
+}
---
+++ BlazorReserva/Data/VueloDetalle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorReserva.Data
+{
+    public class VueloDetalle
+    {
+
+        [Key]
+        public int IdVuelo { get; set; }
+
+        [ForeignKey(nameof(IdVuelo))]
+        public virtual Vuelo? Vuelo { get; set; }
+
+
+
+        public string Destino { get; set; } = "";
+        public int Pasajero { get; set; }
+        public decimal Precio { get; set; }
+
+        [NotMapped]
+        public decimal SubTotal => Pasajero * Precio;
+    }
+}
